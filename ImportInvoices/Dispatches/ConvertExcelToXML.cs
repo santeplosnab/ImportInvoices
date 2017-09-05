5dd6d1397ca7b8cedf5753079e66e1a928c99e37
eshxe <eshxe@mail.ru>
2017-09-05 13:51:32 +0400

    переделал через OleDb

--- ImportInvoices/Dispatches/ConvertExcelToXML.cs
+++ ImportInvoices/Dispatches/ConvertExcelToXML.cs
@@ -1,176 +1,78 @@
-﻿using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Spreadsheet;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Data;
+using System.Data.OleDb;
 using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace ImportInvoices.Dispatches
 {
     class ConvertExcelToXML
     {
         /// <summary>
-        /// Считывание данных из файла Excel в объект DataTable
-        /// </summary>
-        /// <param name="fileName">путь к Excel-файлу</param>
-        /// <returns></returns>
-        private DataTable ReadExcelFile(string fileName)
-        {
-            DataTable dt = new DataTable();
-
-            try
-            {
-                using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
-                {
-                    // получение части книги
-                    WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
-
-                    // получение всех листов выбранной части книги
-                    IEnumerable<Sheet> sheetCollection = spreadsheetDocument.WorkbookPart.Workbook
-                        .GetFirstChild<Sheets>().Elements<Sheet>();
-
-                    // получение идентификатора
-                    string relationshipId = sheetCollection.First().Id.Value;
-
-                    // получение листа 1 книги
-                    WorksheetPart worksheetPart = (WorksheetPart)spreadsheetDocument.WorkbookPart
-                        .GetPartById(relationshipId);
-
-                    // Получание данных
-                    SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
-                    IEnumerable<Row> rowCollection = sheetData.Descendants<Row>();
-
-                    if (rowCollection.Count() == 0)
-                    {
-                        return dt;
-                    }
-
-                    // добавление столбцов
-                    foreach (Cell cell in rowCollection.ElementAt(0))
-                    {
-                        dt.Columns.Add(GetValueOfCell(spreadsheetDocument, cell));
-                    }
-
-                    // Добавление строк в Data Table
-                    foreach (Row row in rowCollection)
-                    {
-                        DataRow tempRow = dt.NewRow();
-                        int columnIndex = 0;
-
-                        foreach (Cell cell in row.Descendants<Cell>())
-                        {
-                            // Получение индекса столбца ячейки
-                            int cellColumnIndex = GetColumnIndex(GetColumnName(cell.CellReference));
-
-                            if (columnIndex < cellColumnIndex)
-                            {
-                                do
-                                {
-                                    tempRow[columnIndex] = string.Empty;
-                                    columnIndex++;
-                                }
-
-                                while (columnIndex < cellColumnIndex);
-                            }
-
-                            tempRow[columnIndex] = GetValueOfCell(spreadsheetDocument, cell);
-                            columnIndex++;
-                        }
-                        // Добавление строки в DataTable
-                        // включая заголовок
-                        dt.Rows.Add(tempRow);
-                    }
-                }
-                // Удаляем строку заголовка
-               // dt.Rows.RemoveAt(0);
-                return dt;
-            }
-            catch (IOException ex)
-            {
-               throw new IOException(ex.Message);
-            }
-        }
-
-        /// <summary>
-        /// Получение значения ячейки
-        /// </summary>
-        /// <param name="spreadsheetDocument"></param>
-        /// <param name="cell">Объект cell</param>
-        /// <returns>Значение в ячейке</returns>
-        private static string GetValueOfCell(SpreadsheetDocument spreadsheetDocument, Cell cell)
-        {
-            // Получение значения в ячейке
-            SharedStringTablePart sharedString = spreadsheetDocument.WorkbookPart.SharedStringTablePart;
-
-            if(cell.CellValue == null)
-            {
-                return string.Empty;
-            }
-
-            string cellValue = cell.CellValue.InnerText;
-
-            // Если тип данных ячейки представлен типом SharedString
-            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
-            {
-                return sharedString.SharedStringTable.ChildElements[int.Parse(cellValue)].InnerText;
-            }
-            else
-            {
-                return cellValue;
-            }
-        }
-
-        /// <summary>
-        /// Получает индекс колонки по ее имени
-        /// </summary>
-        /// <param name="columnName">Column Name(For Example,A or AA)</param>
-        /// <returns>Column Index</returns>
-        private int GetColumnIndex(string columnName)
-        {
-            int columnIndex = 0;
-            int factor = 1;
-
-            // From right to left
-            for (int position = columnName.Length - 1; position >= 0; position--)
-            {
-                // For letters
-                if (Char.IsLetter(columnName[position]))
-                {
-                    columnIndex += factor * ((columnName[position] - 'A') + 1) - 1;
-                    factor *= 26;
-                }
-            }
-
-            return columnIndex;
-        }
-
-        /// <summary>
-        /// Получает Имя колонки по имени ячейки
-        /// </summary>
-        /// <param name="cellReference">Cell Name(For example,A1)</param>
-        /// <returns>Column Name(For example, A)</returns>
-        private string GetColumnName(string cellReference)
-        {
-            // Create a regular expression to match the column name of cell
-            Regex regex = new Regex("[A-Za-z]+");
-            Match match = regex.Match(cellReference);
-            return match.Value;
-        }
-
-        /// <summary>
         /// Конвертирует DataTable в Xml
         /// </summary>
         /// <param name="fileName">Путь к xls файлу</param>
         /// <returns>XML строка</returns>
-        public string GetXML(string fileName)
+        public string GetXML(string filePath)
         {
+            DataTable dataTable = ExcelToDataTable(filePath);
+
             using (DataSet ds = new DataSet())
             {
-                ds.Tables.Add(this.ReadExcelFile(fileName));
+                DataTable dtCopy = dataTable.Copy();
+
+                ds.Tables.Add(dtCopy);
+
                 return ds.GetXml();
             }
         }
+
+        /// <summary>
+        /// Конвертирует excel файл в объект DataTable
+        /// </summary>
+        /// <param name="filePath">Путь к файлу</param>
+        /// <returns>объект DataTable</returns>
+        private static DataTable ExcelToDataTable(string filePath)
+        {
+            string strConn = null;
+
+            if (Path.GetExtension(filePath).Equals(".xls"))
+            {
+                strConn = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"", 
+                    filePath);
+            }
+            else if (Path.GetExtension(filePath).Equals(".xlsx"))
+            {
+                strConn = String.Format("Provider=Microsoft.Ace.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=1\"", 
+                    filePath);
+            }
+            else
+            {
+                return null;
+            }
+
+            DataTable dt = null;
+
+            using (OleDbConnection conn = new OleDbConnection(strConn))
+            {
+                conn.Open();
+
+                DataTable dbSchema = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                string firstSheetName = dbSchema.Rows[0]["TABLE_NAME"].ToString();
+
+                string strExcel = $"select * from [{firstSheetName}]";
+
+                OleDbDataAdapter cmd = new OleDbDataAdapter(strExcel, strConn);
+
+                DataSet ds = new DataSet();
+
+                cmd.Fill(ds, "table");
+
+                dt = ds.Tables["table"];
+            }
+
+            return dt;
+        }
+
     }
 }
